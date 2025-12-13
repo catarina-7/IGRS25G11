2433e470b1fa4e14f896c44a2786de3789ca0255
catarina-7 <152395440+catarina-7@users.noreply.github.com>
2025-12-13 18:50:21 +0000

    Organizar

--- Kamailio/redial.py
+++
@@ -1,9 +0,0 @@
-import ksr
-
-def ksr_request_route():
-    ksr.info("RECEBI UMA MENSAGEM SIP!\n")
-    return 1
-
-def ksr_onreply_route():
-    ksr.info("DETECTEI UMA RESPOSTA SIP!\n")
-    return 1
