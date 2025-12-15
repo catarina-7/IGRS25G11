af71320943906387f9276cf53edf8ba06710357f
MCL-2024 <mariaclara.necas2004@gmail.com>
2025-12-15 00:45:04 +0000

    Adiciona Capturas Wireshark e Logs

--- scripts/redial.py
+++ scripts/redial.py
@@ -23,19 +23,41 @@ class kamailio:
 
         # REGISTER / DEREGISTER
         if msg.Method == "REGISTER":
+
             from_domain = KSR.pv.get("$fd")  # domínio do From-URI
             from_uri = KSR.pv.get("$fu") or "" # AoR do From
             to_uri = KSR.pv.get("$tu") or "" # AoR do To
+
+            contact_hdr = KSR.hdr.get("Contact") or ""
+            expires_hdr = (KSR.hdr.get("Expires") or "").strip()
             
-            KSR.info("REGISTER From: " + str(from_uri) + "domain: " + str(from_domain) + "\n")
+            KSR.info(f"REGISTER From: {from_uri} (domain: {from_domain})\n")
             KSR.info("REGISTER To: " + (to_uri) +
-                     " Contact: " + (KSR.hdr.get("Contact") or "") + "\n") # Extrair AoR do pedido REGISTER (From/To)
+                     " Contact: " + contact_hdr + "\n") # Extrair AoR do pedido REGISTER (From/To)
 
             # Rejeitar utilizadores fora do domínio ACME / Validar domínio do AoR
             if from_domain != ACME_DOMAIN:
-                KSR.info("REGISTER rejected (invalid domain)\n")
-                KSR.sl.send_reply(403, "Forbidden - Invalid Domain")
-                return 1
+                KSR.info("REGISTER rejected (invalid domain)\n") # Log de tentativa rejeitada
+                KSR.sl.send_reply(403, "Forbidden - Invalid Domain") # Rejeitar pedido com resposta SIP adequada (403 Forbidden)
+                return 1 # Garante que nenhum estado ou lista é criado
+            
+            expires = None
+            if expires_hdr.isdigit():
+                expires = int(expires_hdr)
+
+            is_deregister = (expires == 0) or ("expires=0" in contact_hdr.replace(" ", ""))
+            if is_deregister:
+                KSR.info("DEREGISTER detetado para " + (to_uri) + "\n")
+                
+                # Detetar se utilizador está registado antes de remover
+                KSR.pv.sets("$ru", to_uri)
+                is_registered = (KSR.registrar.lookup("location") == 1)
+
+                if not is_registered:
+                    KSR.info("DEREGISTER: utilizador não estava registado: " + (to_uri) + "\n")
+                    # 404 se não existir registo
+                    KSR.sl.send_reply(404, "Not Found (not registered)")
+                    return 1
 
             # Atualizar estado de registo na BD (Guarda (REGISTER ou deregister) no usrloc/location)
             # Responder SIP 200 OK com Contact e expires
